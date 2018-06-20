0237df42435c46e8efbbcaeb1f05808411ca88e3
GowanR <gowan.rowland@gmail.com>
2018-06-19 19:27:33 -0700

    Added Mechanical info expantion.

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
+var isOpenFlag = false;
+
 $(".code-container").hide();
+
+$(".c-container > p").css("maxWidth", "0vw");
+$(".c-container > p").hide();
+
+$(".c-container > div").click(function() {
+  if(isOpenFlag) {
+    $(".c-container > p").animate({maxWidth: "0vw", opacity: "0.0"}, 300, function(){
+      $(".c-container > p").hide();
+    });
+    isOpenFlag = false;
+  } else {
+    $(".c-container > p").show();
+    $(".c-container > p").animate({maxWidth: "15vw", opacity: "1.0"}, 300, function(){});
+    isOpenFlag = true;
+  }
+
+});
+
+
 $(".code-button").click(function() {
   $("body").css({ backgroundColor: "#222222", color: "#ffffff"}, 500);
   $(".main-container").animate({left: '250px', opacity: '0.0'}, 300, function(){
